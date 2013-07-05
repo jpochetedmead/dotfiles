db39a7082a546ea35a8b98a2e4a64cb55d16bd56
Chad Ostrowski <chadoh@chadoh.local>
2013-07-05 17:14:11 -0400

    Good enough to be @chadoh's local config
    I've added in several vim shortcuts I like and simplified the prompt.
    
    setup.sh is now AWESOME. It Just Works, on osx, and is very configurable
    so it can be scripted to work on our servers.
    
    One weird thing, with symlinking the gitconfig: since my .extra sets the
    git user, it modifies .gitconfig. Since that's now symlinked, it ends up
    being set in this project. We'll need to be careful to not accidentally
    set that, but we probably will at some point, anyhow. Let's come up with
    a better solution then.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ PipelineDeals dotfiles
 
 These are dotfiles that are generic enough to use on servers and
 delightful enough for developers to get comfy.
+
+For the vim setup to work, you first need to install [Vundle] by running
+
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+Then once you start vim, install all the plugins by running `:BundleInstall`.
+
+  [Vundle]: https://github.com/gmarik/vundle
--- setup.sh
+++ setup.sh
@@ -1,15 +1,81 @@
 #!/bin/bash
+INSTRUCTIONS="You can probably just run this, on a mac.  By default, it assumes that:
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/bashrc /Users/bhilkert/.bashrc
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/bash_profile /Users/bhilkert/.bash_profile
+ * you're running this file from within the directory where it's saved.
+   If not, set -s with the correct source directory.
+ * you're running this file on an osx-like system where user
+   directories are stored in '/Users'. If not, set -d with the correct user
+   directory path (don't include the actual folder at the end, that's
+   the next option)
+ * you're running this file to set up your own stuff (that is, you're
+   running it as the user you're setting up). If not, you'll need to set
+   -u with the correct username.
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/git-completion.sh /Users/bhilkert/.git-completion.sh
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/gitconfig /Users/bhilkert/.gitconfig
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/gitignore /Users/bhilkert/.gitignore
+You can also run it with -q for quiet-mode (no output on success)."
 
-mkdir -p /Users/bhilkert/.vim-tmp/undo
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/vimrc /Users/bhilkert/.vimrc
+SRC=$PWD
+USER_DIR="/Users"
+USER=`whoami`
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/ack/ackrc /Users/bhilkert/.ackrc
+# options with a colon after them expect an
+# argument, which is stored in $OPTARG
+while getopts ":s:d:u:q" opt; do
+  case $opt in
+    s)
+      # assign a's value to a variable
+      SRC="$OPTARG"
+      ;;
+    d)
+      USER_DIR="$OPTARG"
+      ;;
+    u)
+      USER="$OPTARG"
+      ;;
+    q)
+      QUIET=true
+      ;;
+    ?)
+      echo "$INSTRUCTIONS"
+      exit
+      ;;
+  esac
+done
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/ag/agignore /Users/bhilkert/.agignore
+n=1
+while [ $# -gt 0 ]; do
+  if [ $n -lt $OPTIND ]; then
+    # remove (shift) option arguments
+    # until they are all gone
+    let n=$n+1
+    shift
+  else
+    break;
+  fi
+done
+
+if [ $# -gt 0 ]; then
+  echo "$INSTRUCTIONS"
+  exit
+fi
+
+## WHERE IT ALL GOES DOWN
+ln -fns $SRC/bash/bashrc $USER_DIR/$USER/.bashrc
+ln -fns $SRC/bash/bash_profile $USER_DIR/$USER/.bash_profile
+ln -fns $SRC/bash/functions $USER_DIR/$USER/.functions
+
+ln -fns $SRC/git/git-completion.sh $USER_DIR/$USER/.git-completion.sh
+ln -fns $SRC/git/gitconfig $USER_DIR/$USER/.gitconfig
+ln -fns $SRC/git/gitignore $USER_DIR/$USER/.gitignore
+
+mkdir -p $USER_DIR/$USER/.vim-tmp/undo
+ln -fns $SRC/vim/vimrc $USER_DIR/$USER/.vimrc
+
+ln -fns $SRC/ack/ackrc $USER_DIR/$USER/.ackrc
+
+ln -fns $SRC/ag/agignore $USER_DIR/$USER/.agignore
+# AND THAT'S IT, REALLY
+
+if [ -z $QUIET ]; then
+  echo "Success! Check it out:"
+  ls -l $USER_DIR/$USER/.bashrc $USER_DIR/$USER/.bash_profile $USER_DIR/$USER/.functions $USER_DIR/$USER/.git-completion.sh $USER_DIR/$USER/.gitconfig $USER_DIR/$USER/.gitignore $USER_DIR/$USER/.vimrc $USER_DIR/$USER/.ackrc $USER_DIR/$USER/.agignore
+fi
